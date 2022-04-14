163eb27b7e606f1761f3b4fb3ce40559c7fc1279
Matias Dominguez <petunilo96@gmail.com>
2022-04-13 21:02:15 -0300

    update

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 
 const Item = ({ id, title, price, pictureUrl }) => {
   return (
-    <Link to={`detail/${id}`} style={{ textDecoration: "none", color: "#000" }}>
+    <Link
+      to={`/detail/${id}`}
+      style={{ textDecoration: "none", color: "#000" }}
+    >
       <li className="card">
         <div className="card-img-container">
           <img className="shoe" src={pictureUrl} />
